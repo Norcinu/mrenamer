a0cf738687ab0d2eb1a25a15850e30982ada0a07
Steven Gleed <ledbybyson@gmail.com>
2012-05-17 00:28:29 +0100

    Added some changes. Deleted guff comments.

--- mrenamer.pl
+++ mrenamer.pl
@@ -14,38 +14,35 @@ if ($ARGV[0] ne "") {
     }
 
     opendir(DIR, $work_dir) || die "Could not open directory: $work_dir\n";
-
-    my @music = readdir(DIR);
-    foreach my $i(@music) {
-        my $tags = Audio::Scan->scan_tags($work_dir . $i);
-        print $tags->{tags}->{TPE2}, "\n";
+    
+    my @music;
+    my @directories;
+   
+    while (my $file = readdir(DIR)) {
+        next if($file =~ m/^\./);
+        
+        my $full_path = $work_dir . $file;
+        
+        push (@music, $full_path) if (-f $full_path);
+        push (@directories, $full_path) if (-d $full_path);
     }
+ 
     closedir(DIR);
+
+    foreach my $i(@music) {
+        my $tags = Audio::Scan->scan_tags($i);
+        print $i, "\n";
+        if ($tags->{tags}->{TPE2} eq "" || $tags->{tags}->{TIT2} eq "") {
+            print "null string $i\n";
+        } 
+        else {
+            # will need to change the -4 to find the last . and then extension
+            # because some file formats have 4 char extensions.
+            my $file_extension = substr($work_dir . $i, -4);
+            print $file_extension, "\n";
+            my $new_name = $work_dir . $tags->{tags}->{TPE2} . " " .$tags->{tags}->{TIT2};
+            #rename ($i, $new_name);
+        }
+    }
 }
 
-#print $tags->{tags}->{TFLT}, "\n";
-
-# pass directory path as argument.
-#sub print_msg {
-    #print "mrenamer - renaming files the ipod/itunes likes to mess up.\n";
-#}
-
-#&print_msg;
-
-#$numargs = $#ARGV+1;
-#print "$numargs number of args.\n";
-
-#foreach $argnum (0..$#ARGV) {
- #   print "$ARGV[$argnum] ";
-#}
-
-#print "\n";
-
-#sub print_names {
- #   @names=('Steven Gleed','Emilie Dejon-Stewart', 'Jimmy-Bill from Muswell hill');
-  #  foreach $x (@names) {
-   #     print "$x ";
-    #}
-    #print "\n";
-#}
-#&print_names;
