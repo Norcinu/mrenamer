b3ddf4d46dfe6677d3b13ede8b9d61e20da3fde0
Steven Gleed <ledbybyson@gmail.com>
2012-09-18 16:07:57 +0100

    Made changes to wintable_parser.pl

--- wintable_parser.pl
+++ wintable_parser.pl
@@ -3,26 +3,127 @@
 use strict;
 use warnings;
 
-my @directories = ('wins', 'loses', 'features');
+my @directories = ('\\wins\\', '\\loses\\', '\\features\\');
 my @outcomes;
+my @files; 
+my @file_lengths;
+my %outcomes_to_write;
 
 my $array_prefix = "outcome_";
 my $root_work_dir = $ARGV[0];
 my $output_file = $ARGV[1];
+my $samples = 0;
+my $current_file = 0;
 
 sub ParseDirectory($) {
     opendir(DIR, $_[0]) or die "Cannot open directory.\n";
+	my $line_counter = 0;
+	my $i = 0;
+
+	my @temp;
+#	my @file_lengths;
 
     while (my $file = readdir(DIR)) {
-        open(F, $file);
-        while (<F>) {
+		next if ($file =~ m/^\./);
+		print $file, "\n";
+
+		open(F, $file) or die $!;
+		my @lines;
+		my $outcome_name = '';
+
+		while (<F>) {
             chomp;
-            print "$_\n";
+			$line_counter++;
+			$outcome_name = $array_prefix . $file;
+			my $l = push(@lines, $_);
+			print $l, " ";
+			print @lines[9], " ";
+			#	print $lines[$l];
+			#print "$_\n";
         }
+		
+		$outcomes_to_write{$outcome_name} = [@lines];
+		
+		close(F);
+		
+		push (@file_lengths, $line_counter);
+		#undef (@lines);
+
+		$line_counter = 0;
     }
+
+	closedir(DIR);
 }
 
-sub WriteLines() {
+sub WriteLines($) {
+#	my $array_name = keys $outcomes;
+	my $array_name = '';
+	open(F, '>>outcomes.h');
+
+#	print F $array_name . "[" . $samples. "][15] = {\n";
+	while ((my $key, my $value) = each %outcomes_to_write) {
+		print "$key $value", "\n";
+		#for (my $ctr = 0; $ctr < length($value); $ctr++) {
+			print $outcomes_to_write{$value};
+			#}
+	}
+##	for ($ctr = 0; $ctr < $samples / 3; $ctr++) {
+##		print F %outcomes{$array_name}
+##	}
+	#foreach my $i (keys %outcomes_to_write) {
+	#	print F $i . "\n";
+	#}
+	
+	print F "\n}; // End of $array_name \n\n";
+
+	close(F);
+}
+
+sub SetAverageSampleCap($) {
+	if ($_[0] >= 1000) {
+		$samples = 333*3;
+	}
+	elsif ($_[0] >= 500) {
+		$samples = 166*3;
+	}
+	elsif ($_[0] >= 100) {
+		$samples = 33*3;
+	}
+	elsif ($_[0] >= 30) { 
+		$samples = 30;
+	}
+	elsif ($_[0] >= 10) {
+		$samples = 9;
+	}
+	else {
+		$samples = 1;
+	}
+}
+
+sub SetSampleCap($) {
+	if ($_[0] >= 1000) {
+		$samples = 1000;
+	}
+	elsif ($_[0] >= 500) {
+		$samples = 500;
+	}
+	elsif ($_[0] >= 100) {
+		$samples = 100;
+	}
+	elsif ($_[0] >= 30) { 
+		$samples = 30;
+	}
+	elsif ($_[0] >= 10) {
+		$samples = 10;
+	}
+	else {
+		$samples = 1;
+	}
+}
+
+sub StripExtension() {
 }
 
 ParseDirectory($root_work_dir);
+#WriteLines("temp.txt");
+
